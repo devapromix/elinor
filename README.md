6ca544da7f75cb4a5a192f632a48a792f45b1904
Sergiy Tkach <bees@meta.ua>
2018-03-05 00:13:14 +0200

    Day 3.

--- README.md
+++ README.md
@@ -2,25 +2,32 @@
 Simple roguelike game based on Disciples.
 
 ## Screenshots
-Day 1
+### Day 1
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_1.png)
 
-Day 2
+### Day 2
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_2.png)
 
-Day 3
+### Day 3
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_3.png)
 
-Day 4
+### Day 4
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_4.png)
 
-Day 5
+### Day 5
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_5.png)
 
-Day 6
+### Day 6
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_6.png)
 
-Day 7
+### Day 7
+
 ![screenshot](https://github.com/devapromix-roguelikes/disciplesrl/blob/master/screenshots/screenshot_day_7.png)
 
 ## Current State
--- sources/DisciplesRL.Party.pas
+++ sources/DisciplesRL.Party.pas
@@ -2,6 +2,128 @@ unit DisciplesRL.Party;
 
 interface
 
+uses DisciplesRL.Creatures;
+
+type
+  TRace = (rcTheEmpire, rcNeutrals);
+
+type
+  TPosition = 0 .. 5;
+
+type
+  TParty = class(TObject)
+  private
+    FOwner: TRace;
+    FCreature: array [TPosition] of TCreature;
+    procedure AddCreature(const ACreatureEnum: TCreatureEnum; const APosition: TPosition);
+    function GetCreature(APosition: TPosition): TCreature;
+    procedure SetCreature(APosition: TPosition; const Value: TCreature);
+  public
+    constructor Create;
+    destructor Destroy; override;
+    property Owner: TRace read FOwner write FOwner;
+    property Creature[APosition: TPosition]: TCreature read GetCreature write SetCreature;
+    procedure Clear;
+    function Hire(const ACreatureEnum: TCreatureEnum; const APosition: TPosition): Boolean;
+    procedure Dismiss(const APosition: TPosition);
+    procedure Heal(const APosition: TPosition);
+    procedure Revive(const APosition: TPosition);
+    procedure UpdateHP(const AHitPoints: Integer; const APosition: TPosition);
+    procedure TakeDamage(const ADamage: Integer; const APosition: TPosition);
+  end;
+
 implementation
+
+{ TParty }
+
+procedure TParty.AddCreature(const ACreatureEnum: TCreatureEnum; const APosition: TPosition);
+begin
+  AssignCreature(FCreature[APosition], ACreatureEnum);
+end;
+
+procedure TParty.Clear;
+var
+  I: TPosition;
+begin
+  for I := Low(TPosition) to High(TPosition) do
+    ClearCreature(FCreature[I]);
+end;
+
+constructor TParty.Create;
+begin
+  Self.Clear;
+  Owner := rcNeutrals;
+end;
+
+destructor TParty.Destroy;
+begin
+
+  inherited;
+end;
+
+procedure TParty.Dismiss(const APosition: TPosition);
+begin
+  ClearCreature(FCreature[APosition])
+end;
+
+function TParty.GetCreature(APosition: TPosition): TCreature;
+begin
+  Result := FCreature[APosition]
+end;
+
+procedure TParty.Heal(const APosition: TPosition);
+begin
+  with FCreature[APosition] do
+    if (Active and (HitPoints > 0)) then
+      HitPoints := MaxHitPoints;
+end;
+
+function TParty.Hire(const ACreatureEnum: TCreatureEnum; const APosition: TPosition): Boolean;
+begin
+  Result := False;
+  if not FCreature[APosition].Active then
+  begin
+    Result := True;
+    AddCreature(ACreatureEnum, APosition);
+  end;
+end;
+
+procedure TParty.Revive(const APosition: TPosition);
+begin
+  with FCreature[APosition] do
+    if (Active and (HitPoints = 0)) then
+      HitPoints := 1;
+end;
+
+procedure TParty.SetCreature(APosition: TPosition; const Value: TCreature);
+begin
+  FCreature[APosition] := Value;
+end;
+
+procedure TParty.TakeDamage(const ADamage: Integer; const APosition: TPosition);
+begin
+  with FCreature[APosition] do
+    if Active then
+    begin
+      if (HitPoints > 0) then
+        if (ADamage - Armor > 0) then
+          HitPoints := HitPoints - (ADamage - Armor);
+      if (HitPoints < 0) then
+        HitPoints := 0;
+    end;
+end;
+
+procedure TParty.UpdateHP(const AHitPoints: Integer; const APosition: TPosition);
+begin
+  with FCreature[APosition] do
+    if Active then
+      if (HitPoints > 0) then
+        if (AHitPoints > 0) then
+          if (HitPoints + AHitPoints <= MaxHitPoints) then
+            HitPoints := HitPoints + AHitPoints
+          else
+            HitPoints := MaxHitPoints;
+end;
+
 
 end.
