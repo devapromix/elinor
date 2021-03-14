50791bd4c9cccbf318e61576e26227a18570dd11
devapromix <maxwof@ukr.net>
2021-03-14 10:42:25 +0200

    Items.

--- sources/DisciplesRL.Items.pas
+++ sources/DisciplesRL.Items.pas
@@ -6,8 +6,24 @@ interface
 {$MODESWITCH ADVANCEDRECORDS}
 {$ENDIF}
 
+// Предметы в Д1 -- http://alldisciples.ru/modules.php?name=Articles&pa=showarticle&artid=328
+// Предметы в Д2 -- http://alldisciples.ru/modules.php?name=Articles&pa=showarticle&artid=223
+// Список предм. -- https://www.ign.com/faqs/2005/disciples-ii-rise-of-the-elves-items-listfaq-677342
 uses
   DisciplesRL.Resources;
+
+type
+  TItemType = (itStaff, itElixir, itBook);
+
+type
+  TItemProp = (ipEquipable, ipConsumable, ipReadable);
+
+type
+  TItem = record
+    ItemName: string;
+    ItemType: TItemType;
+    ItemProp: set of TItemProp;
+  end;
 
 implementation
 
