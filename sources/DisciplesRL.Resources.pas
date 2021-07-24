e159f800dc3ec2ab454f563e3e8d907f56d79610
devapromix <maxwof@ukr.net>
2021-07-24 09:52:37 +0300

    Доп. работа с ресурсами.

--- sources/DisciplesRL.Resources.pas
+++ sources/DisciplesRL.Resources.pas
@@ -4,10 +4,13 @@ interface
 
 uses
 {$IFDEF FPC}
-  Graphics;
+  Classes,
+  Graphics,
 {$ELSE}
-  Vcl.Imaging.PNGImage;
+  Vcl.Imaging.PNGImage,
+  System.Classes,
 {$ENDIF}
+  IniFiles;
 {$IFDEF FPC}
 
 type
@@ -474,17 +477,145 @@ const
     //
     );
 
-function GetPath(SubDir: string): string;
+type
+  TResources = class(TObject)
+  public
+    procedure ReadSections(const FileName: string; Sections: TStrings;
+      Section: string = '');
+    function LoadFromFile(const FileName, SectionName, KeyName,
+      DefaultValue: string): string; overload;
+    function LoadFromFile(const FileName, SectionName, KeyName: string;
+      DefaultValue: Integer): Integer; overload;
+    procedure LoadFromFile(const FileName: string;
+      var StringList: TStringList); overload;
+    function KeysCount(const FileName, SectionName: string): Integer;
+    function RandomValue(const FileName, SectionName: string): string;
+    function RandomSectionIdent(const FileName: string): string;
+  end;
 
 implementation
 
 uses
+  Math,
   SysUtils;
 
 function GetPath(SubDir: string): string;
 begin
   Result := ExtractFilePath(ParamStr(0));
   Result := IncludeTrailingPathDelimiter(Result + SubDir);
+end;
+
+function TResources.KeysCount(const FileName, SectionName: string): Integer;
+var
+  IniFile: TMemIniFile;
+  Keys: TStringList;
+begin
+  IniFile := TMemIniFile.Create(GetPath('resources') + FileName + '.ini',
+    TEncoding.UTF8);
+  try
+    Keys := TStringList.Create;
+    try
+      IniFile.ReadSection(SectionName, Keys);
+      Result := Keys.Count;
+    finally
+      FreeAndNil(Keys);
+    end;
+  finally
+    FreeAndNil(IniFile);
+  end;
+end;
+
+function TResources.LoadFromFile(const FileName, SectionName, KeyName: string;
+  DefaultValue: Integer): Integer;
+var
+  IniFile: TMemIniFile;
+begin
+  IniFile := TMemIniFile.Create(GetPath('resources') + FileName + '.ini',
+    TEncoding.UTF8);
+  try
+    Result := IniFile.ReadInteger(SectionName, KeyName, DefaultValue);
+  finally
+    FreeAndNil(IniFile);
+  end;
+end;
+
+procedure TResources.LoadFromFile(const FileName: string;
+  var StringList: TStringList);
+begin
+  StringList.LoadFromFile(GetPath('resources') + FileName + '.txt',
+    TEncoding.UTF8);
+end;
+
+function TResources.LoadFromFile(const FileName, SectionName, KeyName,
+  DefaultValue: string): string;
+var
+  IniFile: TMemIniFile;
+begin
+  Result := DefaultValue;
+  IniFile := TMemIniFile.Create(GetPath('resources') + FileName + '.ini',
+    TEncoding.UTF8);
+  try
+    Result := IniFile.ReadString(SectionName.ToLower, KeyName, DefaultValue)
+      .Trim.Replace('|', #13#10);
+  finally
+    FreeAndNil(IniFile);
+  end;
+end;
+
+function TResources.RandomSectionIdent(const FileName: string): string;
+var
+  FSections: TStringList;
+begin
+  FSections := TStringList.Create;
+  try
+    ReadSections(FileName, FSections);
+    Result := FSections[Math.RandomRange(0, FSections.Count)].Trim;
+  finally
+    FreeAndNil(FSections);
+  end;
+end;
+
+function TResources.RandomValue(const FileName, SectionName: string): string;
+var
+  IniFile: TMemIniFile;
+  Keys: TStringList;
+begin
+  Result := '';
+  IniFile := TMemIniFile.Create(GetPath('resources') + FileName.ToLower +
+    '.ini', TEncoding.UTF8);
+  try
+    Keys := TStringList.Create;
+    try
+      IniFile.ReadSection(SectionName.ToLower, Keys);
+      if Keys.Count = 0 then
+        Exit;
+      Result := LoadFromFile(FileName.ToLower, SectionName.ToLower,
+        Keys[RandomRange(0, Keys.Count)], '');
+    finally
+      FreeAndNil(Keys);
+    end;
+  finally
+    FreeAndNil(IniFile);
+  end;
+end;
+
+procedure TResources.ReadSections(const FileName: string; Sections: TStrings;
+  Section: string = '');
+var
+  IniFile: TMemIniFile;
+  I: Integer;
+begin
+  IniFile := TMemIniFile.Create(GetPath('resources') + FileName + '.ini',
+    TEncoding.UTF8);
+  try
+    IniFile.ReadSections(Sections);
+    if Section <> '' then
+      for I := Sections.Count - 1 downto 0 do
+        if Sections[I].ToLower = Section.ToLower then
+          Sections.Delete(I);
+  finally
+    FreeAndNil(IniFile);
+  end;
 end;
 
 procedure Init;
