8eb4eb105a5dfaf54dec24c0947530bc668d0f42
devapromix <maxwof@ukr.net>
2024-12-03 14:58:49 +0200

    Show frames in frame scene.

--- sources/Scenes/Elinor.Scene.Frames.pas
+++ sources/Scenes/Elinor.Scene.Frames.pas
@@ -3,6 +3,7 @@
 interface
 
 uses
+  Vcl.Graphics,
   Classes,
   DisciplesRL.Button,
   Elinor.Resources,
@@ -11,10 +12,13 @@ uses
 type
   TSceneFrames = class(TScene)
   private
+    FResEnum: TResEnum;
+    FSurface: TBitmap;
   public
-    constructor Create;
+    constructor Create(const AResEnum: TResEnum);
     destructor Destroy; override;
     procedure Render; override;
+    property ResEnum: TResEnum read FResEnum;
   end;
 
 implementation
@@ -25,22 +29,44 @@ uses
 
 { TSceneFrames }
 
-constructor TSceneFrames.Create;
+constructor TSceneFrames.Create(const AResEnum: TResEnum);
+var
+  I, LLeft, LTop, LMid: Integer;
 begin
-  inherited;
+  inherited Create;
+  FResEnum := AResEnum;
+  FSurface := TBitmap.Create;
+  FSurface.Width := ScreenWidth;
+  FSurface.Height := ScreenHeight;
+  FSurface.Canvas.StretchDraw(Rect(0, 0, ScreenWidth, ScreenHeight),
+    ResImage[FResEnum]);
+  LLeft := 10;
+  LTop := TScene.SceneTop;
+  LMid := ScreenWidth - ((320 * 4) + 26);
+  for I := 1 to 12 do
+  begin
+    FSurface.Canvas.Draw(LLeft, LTop, ResImage[reFrame]);
+    Inc(LTop, 120);
+    if LTop > TScene.SceneTop + 240 then
+    begin
+      LTop := TScene.SceneTop;
+      Inc(LLeft, 322);
+      if (I = 6) then
+        Inc(LLeft, LMid);
+    end;
+  end;
 end;
 
 destructor TSceneFrames.Destroy;
 begin
-
+  FreeAndNil(FSurface);
   inherited;
 end;
 
 procedure TSceneFrames.Render;
 begin
   inherited;
-  DrawImage(reWallpaperSettlement);
-  Game.DrawImage(1, 1, reFrame);
+  Game.Surface.Canvas.Draw(0, 0, FSurface);
 end;
 
 end.
