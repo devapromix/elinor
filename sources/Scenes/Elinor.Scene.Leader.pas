d686f37d49f7f46609a01d7fbfc5511b96f7ae26
devapromix <maxwof@ukr.net>
2024-12-20 07:50:14 +0200

    Added files. Refact.

---
+++ sources/Scenes/Elinor.Scene.Leader.pas
@@ -0,0 +1,170 @@
+﻿unit Elinor.Scene.Leader;
+
+interface
+
+uses
+  Elinor.Scene.Menu.Wide,
+  Vcl.Controls,
+  System.Classes,
+  Elinor.Creatures,
+  Elinor.Button,
+  Elinor.Resources,
+  Elinor.Party,
+  Elinor.Scenes;
+
+type
+  TSceneLeader = class(TSceneWideMenu)
+  private
+  public
+    constructor Create;
+    procedure Render; override;
+    procedure Update(var Key: Word); override;
+    procedure Cancel; override;
+    procedure Continue; override;
+    class procedure Show;
+  end;
+
+var
+  RaceCharKind: TRaceCharKind;
+
+implementation
+
+{ TSceneLeader }
+
+uses
+  Math, dialogs,
+  System.SysUtils,
+  Elinor.Scene.Race,
+  DisciplesRL.Scene.Hire,
+  Elinor.Saga,
+  Elinor.Scene.Settlement,
+  Elinor.Scene.Party,
+  Elinor.Frame;
+
+var
+  CurCrEnum: TCreatureEnum;
+
+procedure TSceneLeader.Cancel;
+begin
+  inherited;
+  TSceneRace.Show;
+end;
+
+procedure TSceneLeader.Continue;
+begin
+  inherited;
+  Game.MediaPlayer.PlaySound(mmClick);
+  TSceneHire.CurCrSkillEnum := TCreature.Character(CurCrEnum).SkillEnum;
+  TSaga.Clear;
+  Party[TLeaderParty.LeaderPartyIndex].Owner := TSaga.LeaderRace;
+  Game.MediaPlayer.PlayMusic(mmGame);
+  Game.MediaPlayer.PlaySound(mmExit);
+  TSceneSettlement.Show(stCapital);
+end;
+
+constructor TSceneLeader.Create;
+begin
+  inherited Create(reWallpaperLeader);
+end;
+
+procedure TSceneLeader.Render;
+var
+  LRaceCharKind: TRaceCharKind;
+  LLeft, LTop, X, Y, I, J, N: Integer;
+begin
+  inherited;
+  DrawTitle(reTitleLeader);
+  for LRaceCharKind := Low(TRaceCharKind) to High(TRaceCharKind) do
+  begin
+    LLeft := IfThen(Ord(LRaceCharKind) > 2, TFrame.Col(1), TFrame.Col(0));
+    LTop := IfThen(Ord(LRaceCharKind) > 2, TFrame.Row(Ord(LRaceCharKind) - 3),
+      TFrame.Row(Ord(LRaceCharKind)));
+    with TCreature.Character(Characters[TSaga.LeaderRace][cgLeaders]
+      [LRaceCharKind]) do
+      if HitPoints > 0 then
+      begin
+        DrawUnit(ResEnum, LLeft, LTop, bsCharacter);
+        TSceneParty(Game.GetScene(scParty)).DrawUnitInfo(LLeft, LTop,
+          Characters[TSaga.LeaderRace][cgLeaders][LRaceCharKind], False);
+      end;
+  end;
+
+  RaceCharKind := TRaceCharKind(CurrentIndex);
+  CurCrEnum := Characters[TSaga.LeaderRace][cgLeaders][RaceCharKind];
+
+  if CurCrEnum <> crNone then
+  begin
+    TextLeft := TFrame.Col(2) + 12;
+    TextTop := TFrame.Row(0) + 6;
+    with TCreature.Character(CurCrEnum) do
+    begin
+      AddTextLine(Name[0], True);
+      AddTextLine;
+      AddTextLine('Уровень', Level);
+      AddTextLine('Точность', ChancesToHit);
+      AddTextLine('Инициатива', Initiative);
+      AddTextLine('Здоровье', HitPoints, HitPoints);
+      AddTextLine('Урон', Damage);
+      AddTextLine('Броня', Armor);
+      AddTextLine('Источник', SourceName[SourceEnum]);
+      case ReachEnum of
+        reAny:
+          begin
+            AddTextLine('Дистанция', 'Все поле боя');
+            AddTextLine('Цели', 1);
+          end;
+        reAdj:
+          begin
+            AddTextLine('Дистанция', 'Ближайшие цели');
+            AddTextLine('Цели', 1);
+          end;
+        reAll:
+          begin
+            AddTextLine('Дистанция', 'Все поле боя');
+            AddTextLine('Цели', 6);
+          end;
+      end;
+      for I := 0 to 2 do
+        AddTextLine(Description[I]);
+    end;
+
+  end;
+
+  if CurCrEnum <> crNone then
+  begin
+    TextTop := TFrame.Row(0) + 6;
+    TextLeft := TFrame.Col(3) + 12;
+    AddTextLine('Умения Лидера', True);
+    AddTextLine;
+    AddTextLine(TSkills.Ability(TCreature.Character(CurCrEnum).SkillEnum).Name);
+    for I := 0 to 1 do
+      AddTextLine(TSkills.Ability(TCreature.Character(CurCrEnum).SkillEnum)
+        .Description[I]);
+    AddTextLine;
+    AddTextLine;
+    AddTextLine('Экипировка', True);
+    AddTextLine;
+    AddTextLine(Format('Оружие: %s',
+      [TCreature.EquippedWeapon(TCreature.Character(CurCrEnum).AttackEnum,
+      TCreature.Character(CurCrEnum).SourceEnum)]));
+    AddTextLine;
+    AddTextLine('Скорость Передвижения', TLeaderParty.GetMaxSpeed(CurCrEnum));
+    AddTextLine('Радиус Обзора', TLeaderParty.GetRadius(CurCrEnum));
+    AddTextLine('Заклинаний в день', TLeaderParty.GetMaxSpells(CurCrEnum));
+  end;
+
+end;
+
+class procedure TSceneLeader.Show;
+begin
+  Game.MediaPlayer.PlaySound(mmClick);
+  Game.Show(scLeader);
+end;
+
+procedure TSceneLeader.Update(var Key: Word);
+begin
+  inherited;
+
+end;
+
+end.
---
+++ sources/Scenes/Elinor.Scene.Menu.Wide.pas
@@ -0,0 +1,209 @@
+﻿unit Elinor.Scene.Menu.Wide;
+
+interface
+
+uses
+  System.Classes,
+  Vcl.Controls,
+  Elinor.Resources,
+  Elinor.Button,
+  Elinor.Scene.Frames;
+
+type
+  TSceneWideMenu = class(TSceneFrames)
+  private type
+    TTwoButtonEnum = (btCancel, btContinue);
+  private const
+    TwoButtonText: array [TTwoButtonEnum] of TResEnum = (reTextCancel,
+      reTextContinue);
+  private
+    TwoButton: array [TTwoButtonEnum] of TButton;
+    FCurrentIndex: Integer;
+    procedure RenderButtons;
+    public var
+  public
+    constructor Create(const AResEnum: TResEnum);
+    destructor Destroy; override;
+    procedure Render; override;
+    procedure MouseMove(Shift: TShiftState; X, Y: Integer); override;
+    procedure MouseDown(AButton: TMouseButton; Shift: TShiftState;
+      X, Y: Integer); override;
+    procedure Cancel; virtual;
+    procedure Continue; virtual;
+    property CurrentIndex: Integer read FCurrentIndex write FCurrentIndex;
+    procedure Update(var Key: Word); override;
+    procedure Basic(AKey: Word);
+  end;
+
+implementation
+
+uses
+  Math,
+  SysUtils,
+  Elinor.Frame,
+  Elinor.Common,
+  Elinor.Scenes;
+
+{ TSceneSimpleMenu }
+
+procedure TSceneWideMenu.Basic(AKey: Word);
+begin
+  case AKey of
+    K_ESCAPE:
+      Cancel;
+    K_ENTER:
+      Continue;
+  end;
+end;
+
+procedure TSceneWideMenu.Cancel;
+begin
+
+end;
+
+procedure TSceneWideMenu.Continue;
+begin
+
+end;
+
+constructor TSceneWideMenu.Create(const AResEnum: TResEnum);
+var
+  LTwoButtonEnum: TTwoButtonEnum;
+  LLeft, LWidth: Integer;
+begin
+  inherited Create(AResEnum, fgLS6, fgRM2);
+  LWidth := ResImage[reButtonDef].Width + 4;
+  LLeft := ScrWidth - ((LWidth * (Ord(High(TTwoButtonEnum)) + 1)) div 2);
+  for LTwoButtonEnum := Low(TTwoButtonEnum) to High(TTwoButtonEnum) do
+  begin
+    TwoButton[LTwoButtonEnum] := TButton.Create(LLeft, DefaultButtonTop,
+      TwoButtonText[LTwoButtonEnum]);
+    Inc(LLeft, LWidth);
+    if (LTwoButtonEnum = btContinue) then
+      TwoButton[LTwoButtonEnum].Sellected := True;
+  end;
+end;
+
+destructor TSceneWideMenu.Destroy;
+var
+  LTwoButtonEnum: TTwoButtonEnum;
+begin
+  for LTwoButtonEnum := Low(TTwoButtonEnum) to High(TTwoButtonEnum) do
+    FreeAndNil(TwoButton[LTwoButtonEnum]);
+  inherited;
+end;
+
+procedure TSceneWideMenu.MouseDown(AButton: TMouseButton; Shift: TShiftState;
+  X, Y: Integer);
+var
+  I: Integer;
+begin
+  inherited;
+  case AButton of
+    mbLeft:
+      begin
+        for I := 0 to 2 do
+          if MouseOver(TFrame.Col(1), TFrame.Row(I), X, Y) then
+          begin
+            Game.MediaPlayer.PlaySound(mmClick);
+            CurrentIndex := I;
+            Break;
+          end;
+        begin
+          if TwoButton[btCancel].MouseDown then
+            Cancel;
+          if TwoButton[btContinue].MouseDown then
+            Continue;
+        end;
+      end;
+  end;
+end;
+
+procedure TSceneWideMenu.MouseMove(Shift: TShiftState; X, Y: Integer);
+var
+  LTwoButtonEnum: TTwoButtonEnum;
+begin
+  inherited;
+  for LTwoButtonEnum := Low(TTwoButtonEnum) to High(TTwoButtonEnum) do
+    TwoButton[LTwoButtonEnum].MouseMove(X, Y);
+end;
+
+procedure TSceneWideMenu.Render;
+var
+  LX, LY: Integer;
+begin
+  inherited;
+
+  case CurrentIndex of
+    0 .. 2:
+      begin
+        LX := TFrame.Col(0);
+        LY := TFrame.Row(CurrentIndex);
+      end;
+    3 .. 5:
+      begin
+        LX := TFrame.Col(1);
+        LY := TFrame.Row(CurrentIndex - 3);
+      end;
+  end;
+  DrawImage(LX, LY, reActFrame);
+
+  RenderButtons;
+end;
+
+procedure TSceneWideMenu.RenderButtons;
+var
+  LTwoButtonEnum: TTwoButtonEnum;
+begin
+  for LTwoButtonEnum := Low(TTwoButtonEnum) to High(TTwoButtonEnum) do
+    TwoButton[LTwoButtonEnum].Render;
+end;
+
+procedure TSceneWideMenu.Update(var Key: Word);
+var
+  FF: Boolean;
+begin
+  inherited;
+  FF := CurrentIndex in [0 .. 4];
+  case Key of
+    K_ESCAPE:
+      Cancel;
+    K_ENTER:
+      if FF then
+        Continue;
+    K_UP:
+      begin
+        Game.MediaPlayer.PlaySound(mmClick);
+        case CurrentIndex of
+          1, 2, 4, 5:
+            CurrentIndex := CurrentIndex - 1;
+        end;
+      end;
+    K_Down:
+      begin
+        Game.MediaPlayer.PlaySound(mmClick);
+        case CurrentIndex of
+          0, 1, 3, 4:
+            CurrentIndex := CurrentIndex + 1;
+        end;
+      end;
+    K_LEFT:
+      begin
+        Game.MediaPlayer.PlaySound(mmClick);
+        case CurrentIndex of
+          3 .. 5:
+            CurrentIndex := CurrentIndex - 3;
+        end;
+      end;
+    K_RIGHT:
+      begin
+        Game.MediaPlayer.PlaySound(mmClick);
+        case CurrentIndex of
+          0 .. 2:
+            CurrentIndex := CurrentIndex + 3;
+        end;
+      end;
+  end;
+end;
+
+end.
