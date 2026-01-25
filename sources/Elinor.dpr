a52376e18909a2e7b1c124eedb90961b26aac94a
devapromix <maxwof@ukr.net>
2026-01-25 10:30:03 +0200

    Refactor.

--- sources/Elinor.dpr
+++ sources/Elinor.dpr
@@ -30,7 +30,7 @@ uses
   Elinor.Scene.Spellbook in 'Scenes\Elinor.Scene.Spellbook.pas',
   Elinor.MediaPlayer in 'Game\Elinor.MediaPlayer.pas',
   Elinor.PathFind in 'Elinor.PathFind.pas',
-  Elinor.Spells in 'Elinor.Spells.pas',
+  Elinor.Spells in 'Game\Elinor.Spells.pas',
   Elinor.Treasure in 'Elinor.Treasure.pas',
   Elinor.Statistics in 'Game\Elinor.Statistics.pas',
   Elinor.Scene.Frames in 'Scenes\Elinor.Scene.Frames.pas',
